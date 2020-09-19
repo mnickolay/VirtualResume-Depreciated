1d7cbcbfcc050e946d47b5c18457f45480ebd96a
Matthew Nickolay <nickolaymatt@gmail.com>
2020-09-19 10:55:20 -0500

    switched stylings of home and about. adding in icons to contact

--- scripts/home.js
+++ scripts/home.js
@@ -1,81 +1,35 @@
-class Circle {
-    constructor(dom, xCoor, yCoor){
-        this.dom = dom;
-        this.xCoor = xCoor;
-        this.yCoor = yCoor;
-    }
-}
-var colorStorage = document.querySelector("#colorStorage").innerText;
-var svgCircles = document.querySelector("#svgCircles");
-var html = document.querySelector("html");
+// class Quad{
+//     constructor(dom, xCoor, yCoor){
+//         this.dom = dom;
+//         this.xCoor = xCoor;
+//         this.yCoor = yCoor;
+//     }
+// }
 
-createCircleOnPage();
+// var quad1 = document.querySelector("#borderBoxB"),
+//     quad2 = document.querySelector("#colorBoxL"),
+//     quad3 = document.querySelector("#colorBoxR"),
+//     quad4 = document.querySelector("#aboutHeadPara"),
+//     quad5 = document.querySelector("#borderBoxF");
 
-var circle1 = document.querySelector("#circle1"),
-    circle2 = document.querySelector("#circle2"),
-    circle3 = document.querySelector("#circle3"),
-    circle4 = document.querySelector("#circle4"),
-    circle5 = document.querySelector("#circle5"),
-    circle6 = document.querySelector("#circle6"),
-    circle7 = document.querySelector("#circle7");
+// var q1 = new Quad(quad1, window.getComputedStyle(quad1).getPropertyValue("left"), window.getComputedStyle(quad1).getPropertyValue("top")),
+//     q2 = new Quad(quad2, window.getComputedStyle(quad2).getPropertyValue("left"), window.getComputedStyle(quad2).getPropertyValue("top")),
+//     q3 = new Quad(quad3, window.getComputedStyle(quad3).getPropertyValue("left"), window.getComputedStyle(quad3).getPropertyValue("top")),
+//     q4 = new Quad(quad4, window.getComputedStyle(quad4).getPropertyValue("left"), window.getComputedStyle(quad4).getPropertyValue("top")),
+//     q5 = new Quad(quad5, window.getComputedStyle(quad5).getPropertyValue("left"), window.getComputedStyle(quad5).getPropertyValue("top"));
 
-var c1 = new Circle(circle1, circle1.getAttribute("cx"), circle1.getAttribute("cy")),
-    c2 = new Circle(circle2, circle2.getAttribute("cx"), circle2.getAttribute("cy")),
-    c3 = new Circle(circle3, circle3.getAttribute("cx"), circle3.getAttribute("cy")),
-    c4 = new Circle(circle4, circle4.getAttribute("cx"), circle4.getAttribute("cy")),
-    c5 = new Circle(circle5, circle5.getAttribute("cx"), circle5.getAttribute("cy")),
-    c6 = new Circle(circle6, circle6.getAttribute("cx"), circle6.getAttribute("cy")),
-    c7 = new Circle(circle7, circle7.getAttribute("cx"), circle7.getAttribute("cy"));
-    
-var circleObjArr = [c1, c2, c3, c4, c5, c6, c7];
+// var quadArr = [q1,q2,q3,q4,q5];
 
-function createCircleOnPage(){
-    for(var i=1;i<8;i++){
-        var cx = rngCircleXandY();
-        var cy = rngCircleXandY();
-        var r = rngCircleRadius();
-        var id = i.toString();
-        var str = createCircleString(cx, cy, r, id, i);
+// var html = document.querySelector("html");
 
-        svgCircles.innerHTML += str;
-        circleMovementEvent(document.querySelector("#circle" + i));
-    }
-}
+// html.addEventListener("mousemove", e =>{
+//     thisX = parseInt(e.clientX);
+//     thisY = parseInt(e.clientY);
 
-function createCircleString(cx, cy, r, num){
-    var str = "<circle fill='" + colorStorage + "' ";
-    str += "cx='" + cx + "' ";
-    str += "cy='" + cy + "' ";
-    str += "r='" + r + "'";
-    str += "class='circle' "
-    str += "id='circle" + num + "'/>"
-    return str;
-}
-
-function rngCircleXandY(){
-    return Math.floor((Math.random() * 900) + 50)
-}
-
-function rngCircleRadius(){
-    return Math.floor((Math.random() * 110) + 10)
-}
-
-function circleMovementEvent(thisCircle){
-    html.addEventListener("mousemove", e =>{
-        thisX = parseInt(e.clientX);
-        thisY = parseInt(e.clientY);
-        var count = 1;
-
-        circleObjArr.forEach(function(cir){
-            var xcoord=thisX/30*count + parseInt(cir.xCoor);
-            var ycoord=thisY/30*count + parseInt(cir.yCoor);
-            cir.dom.setAttribute("cx", xcoord);
-            cir.dom.setAttribute("cy", ycoord);
-            count+=0.2;
-        })
-    })
-}
-
-circleObjArr.forEach(function(cir){
-    cir.dom.setAttribute("fill", rgba());
-})
+//     quadArr.forEach(function(q){
+//         var xcoord=thisX/30 + parseInt(q.xCoor);
+//         var ycoord=thisY/30 + parseInt(q.yCoor);
+//         q.dom.style.left = xcoord;
+//         q.dom.style.top = ycoord;
+//     })
+// })
